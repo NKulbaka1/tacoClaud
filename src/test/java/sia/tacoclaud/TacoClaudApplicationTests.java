3cde4d19a92651f663958b5ba8d5ccfc698f765e
NKulbaka <nikita.kulbaka@mail.ru>
2024-06-06 17:32:35 +0300

    Initial commit

---
+++ src/test/java/sia/tacoclaud/TacoClaudApplicationTests.java
@@ -0,0 +1,13 @@
+package sia.tacoclaud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoClaudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
